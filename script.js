0aff357fa1e16a87d0e906397a42d56cf1f4bfd8
davidgomezcode <davidgomezcode@gmail.com>
2024-07-19 19:36:51 -0500

    Adding of some images

--- script.js
+++ script.js
@@ -5,15 +5,19 @@ const profileImg = document.querySelector(".presentation-img");
 const davidProfile = document.querySelector(".davidProfile");
 const btnCloseDavidProfile = document.querySelector(".close--presentation-img");
 const scrollBackToTheTop = document.querySelector(".scrollBackToTheTop");
+const firstParagraph = document.querySelector(".firstParagraph");
+const lawyerWord = document.querySelector(".lawyerWord");
+const lawyerImage = document.querySelector(".lawyerImage");
 
-//Buttons:
-davidProfile.addEventListener("click", function () {
-  profileImg.classList.toggle("hidden");
+//Listener to hide or unhide the lawyer image:
+lawyerWord.addEventListener("click", function () {
+  lawyerImage.classList.toggle("hidden");
 });
-btnCloseDavidProfile.addEventListener("click", function () {
-  profileImg.classList.toggle("hidden");
+lawyerImage.addEventListener("click", function () {
+  lawyerImage.classList.toggle("hidden");
 });
 
+//Button to scroll back to the top:
 scrollBackToTheTop.addEventListener("click", function () {
   davidProfile.scrollIntoView({ behavior: "smooth" });
 });
