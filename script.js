5bcf0203e55b2b56282ec0403dade02e30fdcf93
davidgomezcode <davidgomezcode@gmail.com>
2024-07-31 18:38:04 -0500

    I add the slow appearing of the sections

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const scrollBackToTheTop = document.querySelector(".scrollBackToTheTop");
 const firstParagraph = document.querySelector(".firstParagraph");
 const lawyerWord = document.querySelector(".lawyerWord");
 const lawyerImage = document.querySelector(".lawyerImage");
+const sections = document.querySelectorAll(".section");
 
 //Listener to hide or unhide the lawyer image:
 lawyerWord.addEventListener("click", function () {
@@ -44,4 +45,28 @@ new IntersectionObserver(stickyImg, optionsStickyImg).observe(firstParagraph);
 
 presentationImgIMG.addEventListener("click", function () {
   davidProfile.scrollIntoView({ behavior: "smooth" });
+});
+
+// IntersectionObserver in the sections to slow-show them as the user scroll down:
+
+const showSlow = function (e, observer) {
+  console.log(e[0].isIntersecting);
+  if (e[0].isIntersecting) {
+    e[0].target.classList.remove("hidden"); // we remove the class hidden, so the section is shown.
+    observer.unobserve(e[0].target); // we stop observing the section that is shown
+  }
+
+  // console.log(observer);
+};
+const optionsShowSlow = {
+  root: null,
+  threshold: 0.3, // the callback function is executed when 60% of the section is visible
+};
+
+const sectionObserver = new IntersectionObserver(showSlow, optionsShowSlow);
+
+sections.forEach((section) => {
+  //at the beginning:
+  section.classList.add("hidden"); //all sections are hidden
+  sectionObserver.observe(section); // We use the same observer to observe each section.
 });
